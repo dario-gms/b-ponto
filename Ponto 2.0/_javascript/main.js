3f7157325f0ecda47c616b9c42426a702ff5b5e1
dario-gms <dario.gms@hotmail.com.br>
2021-09-02 17:01:03 -0300

    Site ponto atualizado

--- Ponto 2.0/_javascript/main.js
+++ Ponto 2.0/_javascript/main.js
@@ -1,53 +1,44 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+moment.locale('pt-br')
+var hour = moment().format('LT')
+var date = new Date();
+var time = date.getHours();
+var user = 'Dario';
+function load() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('image');
     
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    msg.innerHTML = `Agora são <strong>${hora}:${minuto}</strong>.`
-    if (hora >= 0 && hora < 12) {
-        img.src = '_imagens/manhã.png'
-        document.body.style.background = '#1fa64c'
-        msg.innerHTML += '<p>Tenha um bom dia.</p>'
-    } else if (hora >= 12 && hora < 18) {
-        img.src = '_imagens/tarde.png'
-        document.body.style.background = '#6693c4'
-        msg.innerHTML += '<p>Tenha uma boa tarde</p>'
+    
+    msg.innerHTML = `Agora são <strong>${hour}</strong>.`;
+    if (time >= 0 && time < 12) {
+        img.src = '_imagens/manha.png';
+        document.body.style.background = '#1fa64c';
+        msg.innerHTML += '<p>Tenha um bom dia.</p>';
+    } else if (time >= 12 && time < 18) {
+        img.src = '_imagens/tarde.png';
+        document.body.style.background = '#6693c4';
+        msg.innerHTML += '<p>Tenha uma boa tarde</p>';
     } else {
-        img.src = '_imagens/noite.png'
-        document.body.style.background = '#0e151d'
-        msg.innerHTML += '<p>Tenha uma boa noite</p>'
+        img.src = '_imagens/noite.png';
+        document.body.style.background = '#0e151d';
+        msg.innerHTML += '<p>Tenha uma boa noite</p>';
     }
 }
-function marcar() {
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    var ini;
-    user = window.prompt("Digite seu usuário");
-    var acess = window.prompt("Digite seu código de acesso")
-    if (user == 'Dario' && acess == 994936694) {
-        ini = `Expediente iniciado às <strong>${hora}:${minuto}</strong>. \nTenha um bom trabalho <strong>${user}</strong>!`;
-    } else if (user || acess != true) {
-        alert("Insira o usuário e código de acesso corretamente.")
-        ini = "Expediente não iniciado.";
-    }
-    document.getElementById("inicio").innerHTML = ini;
+function mark() {
+    start = `Expediente iniciado às <strong>${hour}</strong>. \nTenha um bom trabalho <strong>${user}</strong>!`;
+    document.getElementById("start").innerHTML = start;
 }
 
-function encerrar() {
-    var data = new Date()
-    var hora = data.getHours()
-    var minuto = data.getMinutes()
-    var fim;
+function terminate() {
     var r = confirm("Deseja encerrar o expediente?");
     if (r == true) {
-        fim = `Expediente encerrado às <strong>${hora}:${minuto}</strong>. \nBom descanso <strong>${user}</strong>!`;
+        finish = `Expediente encerrado às <strong>${hour}</strong>. \nBom descanso <strong>${user}</strong>!`;
     }
     else {
-        alert("Ação cancelada!")
-        fim = "Tenha um bom trabalho.";
+        alert("Ação cancelada!");
+        finish = "Tenha um bom trabalho.";
     }
-    document.getElementById("final").innerHTML = fim;
+    document.getElementById("finish").innerHTML = finish;
 }
+
+
+
