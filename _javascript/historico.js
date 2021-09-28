8a5a7ab15b53a973ed14ccf42e8163db930c9fee
dario-gms <dario.gms@hotmail.com.br>
2021-09-28 16:19:27 -0300

    Atualização de pasta

---
+++ _javascript/historico.js
@@ -0,0 +1,52 @@
+// var records = [
+//     {
+//         local: moment.locale('pt-br'),
+//         date: getDate(),
+//         entrance: localStorage.getItem('start'),
+//         exit: localStorage.getItem('finish')
+//     }   
+// ];
+moment.locale('pt-br')
+let dayWeek = moment().format('LL');
+document.getElementById("dayWeek").innerHTML = dayWeek;
+let retrievedObject = localStorage.getItem('testObject');
+let records = JSON.parse(retrievedObject);
+
+function addTable() {
+    let tBody = document.getElementById("minhaTabela");
+    
+    for (record in records) {
+        let tr = addLine(records[record]);
+        tBody.appendChild(tr);
+    }
+}
+addTable();
+
+function getDate() {
+    let result = localStorage.getItem('date');
+    if (!result) {
+        result = "N/A";
+    }
+    return result;
+}
+
+function addLine(record) {
+    let tr = document.createElement('td');
+    // let tdDate = addCell(record, "date");
+    // tr.appendChild(tdDate);
+    let tdEntrada = addCell(record, "entrance");
+    tr.appendChild(tdEntrada);
+    // let tdSaida = addCell(record, "exit");
+    // tr.appendChild(tdSaida);
+    return tr;
+}
+
+function addCell(record, nameItem) {
+    // let border = '2px solid black';
+    let td = document.createElement('button');
+    let date = record[nameItem];
+    td.appendChild(document.createTextNode(date));
+    // td.style.border = border;
+    return td;
+}
+
---
+++ _javascript/main.js
@@ -0,0 +1,78 @@
+moment.locale('pt-br')
+var hour = moment().format('LT')
+var date = new Date();
+var time = date.getHours();
+var user = 'Dario';
+function load() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('image');
+    msg.innerHTML = `Agora são <strong>${hour}</strong>.`;
+    if (time >= 0 && time < 12) {
+        img.src = '_imagens/manha.png';
+        document.body.style.background = '#1fa64c';
+        msg.innerHTML += '<p>Tenha um bom dia.</p>';
+    } else if (time >= 12 && time < 18) {
+        img.src = '_imagens/tarde.png';
+        document.body.style.background = '#6693c4';
+        msg.innerHTML += '<p>Tenha uma boa tarde!</p>';
+        if (time >=12 && time < 14){
+            msg.innerHTML += 'Que tal uma pausa para o almoço?'
+        }
+    } else {
+        img.src = '_imagens/noite.png';
+        document.body.style.background = '#0e151d';
+        msg.innerHTML += '<p>Tenha uma boa noite.</p>';
+    }
+}
+function mark() {
+    document.getElementById("start").innerHTML = "Marcação realizada com sucesso!"
+        let retrievedObject = localStorage.getItem('testObject');
+        var records = JSON.parse(retrievedObject)
+
+
+        let testObject = {
+            date: moment().format('L'),
+            entrance: moment().format('LT'),
+        };
+
+        if (records) {
+            records.push(testObject);
+        } else {
+            records = [testObject]
+        }
+
+
+        // var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+        // Put the object into storage
+        localStorage.setItem('testObject', JSON.stringify(records));
+        
+        // Retrieve the object from storage
+        
+        // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+
+
+
+
+        // localStorage.setItem('reg', reg);
+        // if (localStorage.mark) {
+        //     document.getElementById('start').innerHTML = localStorage.mark;
+        // }
+}
+
+// function terminate() {
+//     var reply = confirm("Deseja encerrar o expediente?");
+//     localStorage.setItem('finish', moment().format('LT'));
+//     if (reply) {
+//         finish = `Expediente encerrado às <strong>${localStorage.getItem('finish')}</strong>. \nBom descanso <strong>${user}</strong>!`;
+//     }
+//     else {
+//         alert("Ação cancelada!");
+//         finish = "Tenha um bom trabalho.";
+//     }
+//     document.getElementById("finish").innerHTML = finish;
+// }
+
+
+
